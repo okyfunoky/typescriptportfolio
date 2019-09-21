bd4949e70c7b15168f2981a101c22840201ba004
Michael Stevens <okyfunoky@hotmail.com>
2019-09-21 15:40:48 -0700

    Starting to display projects

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -10,9 +10,9 @@ var Project = /** @class */ (function () {
     return Project;
 }());
 var Projects = [];
-var projectOne = new Project("Marvel Hangman", "link", "pictureLink", "A JavaScript hangman game with a Marvel theme", "Tony Stark: Iron Man");
-var projectTwo = new Project("Where in the World is Karma Sandio?", "link", "pictureLink", "A map based geography game that totally doesn't violate copyrights.", "Karma Sandio Logo: She wears a big grey hat");
-var projectThree = new Project("Marvel RPG", "link", "pictureLink", "Role playing game to determine the strongest Avenger.", "Thor: Strongest Avenger");
+var projectOne = new Project("Marvel Hangman", "link", "./assets/images/ironman.jpg", "A JavaScript hangman game with a Marvel theme", "Tony Stark: Iron Man");
+var projectTwo = new Project("Where in the World is Karma Sandio?", "link", "./assets/images/logo.png", "A map based geography game that totally doesn't violate copyrights.", "Karma Sandio Logo: She wears a big grey hat");
+var projectThree = new Project("Marvel RPG", "link", "./assets/images/thor.jpg", "Role playing game to determine the strongest Avenger.", "Thor: Strongest Avenger");
 //let projectFour = new Project("Marvel RPG","link","pictureLink","Role playing game to determine the strongest Avenger.");
 Projects.push(projectOne);
 Projects.push(projectTwo);
@@ -22,10 +22,21 @@ Projects.push(projectThree);
 function displayProjects() {
     var projectDiv = $('#projectlist');
     Projects.forEach(function (project) {
-        var newProject = $('<img>');
-        newProject.attr("src", project.pictureUrl);
-        newProject.attr("alt", project.pictureAlt);
-        projectDiv.append(newProject);
+        var projectCard = $('<div>').attr("class", "card card-rotating text-center");
+        var imageDiv = $("<div>").attr("class", "card-up");
+        var image = $('<img>').attr("class", "");
+        image.attr("src", project.pictureUrl);
+        image.attr("alt", project.pictureAlt);
+        image.attr("height", "200em");
+        image.attr("width", "200em");
+        var cardBody = $('<div>').attr("class", "card-body");
+        var projectTitle = $("<h4>").attr("class", "font-weight-bold mb-3");
+        projectTitle.text(project.name);
+        projectCard.append(imageDiv);
+        projectCard.append(projectTitle);
+        imageDiv.append(image);
+        projectCard.append(cardBody);
+        projectDiv.append(projectCard);
     });
 }
 displayProjects();
--- assets/scripts/logic.ts
+++ assets/scripts/logic.ts
@@ -7,7 +7,7 @@ class Project {
     description: string;
     pictureAlt: string;
 
-    constructor(projectName: string, projectLink: string, projectPictureUrl: string, projectDescription: string, projectPictureAlt){
+    constructor(projectName: string, projectLink: string, projectPictureUrl: string, projectDescription: string, projectPictureAlt: string){
         this.name = projectName;
         this.link = projectLink;
         this.pictureUrl = projectPictureUrl;
@@ -18,9 +18,9 @@ class Project {
 
 let Projects: Array<Project> = [];
 
-let projectOne = new Project("Marvel Hangman","link","pictureLink","A JavaScript hangman game with a Marvel theme", "Tony Stark: Iron Man");
-let projectTwo = new Project("Where in the World is Karma Sandio?","link","pictureLink","A map based geography game that totally doesn't violate copyrights.", "Karma Sandio Logo: She wears a big grey hat");
-let projectThree = new Project("Marvel RPG","link","pictureLink","Role playing game to determine the strongest Avenger.", "Thor: Strongest Avenger");
+let projectOne = new Project("Marvel Hangman","link","./assets/images/ironman.jpg","A JavaScript hangman game with a Marvel theme", "Tony Stark: Iron Man");
+let projectTwo = new Project("Where in the World is Karma Sandio?","link","./assets/images/logo.png","A map based geography game that totally doesn't violate copyrights.", "Karma Sandio Logo: She wears a big grey hat");
+let projectThree = new Project("Marvel RPG","link","./assets/images/thor.jpg","Role playing game to determine the strongest Avenger.", "Thor: Strongest Avenger");
 //let projectFour = new Project("Marvel RPG","link","pictureLink","Role playing game to determine the strongest Avenger.");
 
 Projects.push(projectOne);
@@ -29,14 +29,24 @@ Projects.push(projectThree);
 //Projects.push(projectFour);
 
 //display Projects
-function displayProjects(){
-    
-    let projectDiv = $('#projectlist');
-    Projects.forEach(project => {
-        let newProject = $('<img>');
-        newProject.attr("src",project.pictureUrl);
-        newProject.attr("alt",project.pictureAlt);
-        projectDiv.append(newProject);
+function displayProjects() {
+    var projectDiv = $('#projectlist');
+    Projects.forEach(function (project) {
+        let projectCard = $('<div>').attr("class","card card-rotating text-center");
+        let imageDiv = $("<div>").attr("class","card-up");
+        let image = $('<img>').attr("class","");
+        image.attr("src", project.pictureUrl);
+        image.attr("alt", project.pictureAlt);
+        image.attr("height","200em"); 
+        image.attr("width","200em"); 
+        let cardBody = $('<div>').attr("class","card-body");
+        let projectTitle = $("<h4>").attr("class","font-weight-bold mb-3");
+        projectTitle.text(project.name);
+        projectCard.append(imageDiv);
+        projectCard.append(projectTitle);
+        imageDiv.append(image);
+        projectCard.append(cardBody);
+        projectDiv.append(projectCard);
     });
 }
 displayProjects();
